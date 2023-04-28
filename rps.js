247f86ec5485c8c40d049be253519f325d04f13c
Brett Stine <r.brettstine@gmail.com>
2023-04-27 21:14:32 -0400

    Implement a Score limit and a replay button
     - Log from each round, and scoreboard reset on replay
     - various style, HTML changes

--- rps.js
+++ rps.js
@@ -30,15 +30,44 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function checkEndGame(scoreBoard) {
+    if (scoreBoard[0]>=5) {
+        return "You Win!";
+    }
+    if (scoreBoard[1]>=5) {
+        return "Computer Wins";
+    }
+    return false;
+    
+}
+
 function game() {
     const choiceButtons = document.querySelectorAll('.choice');
     const resultsDiv = document.querySelector('#results');
     const playerScore = document.querySelector('#playerScore');
     const computerScore = document.querySelector('#computerScore');
+    const finalResultContainer = document.querySelector('#finalResultContainer');
+    const finalResult = document.querySelector('#finalResult');
+    const replayButton = document.querySelector('#replay');
     
     let roundCounter = 0;
     let scoreBoard = [0, 0, 0]; //win, loss, tie
     let roundResult = "";
+    let isEndGame = false;
+
+    replayButton.addEventListener('click', function() {
+        removeAllChildNodes(resultsDiv);
+        scoreBoard = [0,0,0];
+        finalResultContainer.hidden = true;
+        playerScore.textContent = scoreBoard[0];
+        computerScore.textContent = scoreBoard[1];
+    });
 
     for (const choiceButton of choiceButtons) {
 
@@ -58,6 +87,13 @@ function game() {
             newResult.textContent = roundResult;
 
             resultsDiv.appendChild(newResult);
+            
+            isEndGame = checkEndGame(scoreBoard);
+
+            if (isEndGame) {
+                finalResult.textContent = isEndGame;
+                finalResultContainer.hidden = false;
+            };
         });
     };
     
